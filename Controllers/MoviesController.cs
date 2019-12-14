df94452d2aa470250384ac0c96ad5609fc02c31c
anshulv1401 <31562315+anshulv1401@users.noreply.github.com>
2019-12-15 00:58:07 +0530

    General-Web UI-Dev process-Service all with parameters

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            //return View(movie);
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int movieID)
+        {
+            return Content("id = " + movieID);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(string.Format("PageIndex  : {0}, Sortby : {1}", pageIndex, sortBy));
+        }
+    }
+}
