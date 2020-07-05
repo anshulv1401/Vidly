8f93419417f117cc8f82deaafd04abb0eaa82654
anshulv1401 <31562315+anshulv1401@users.noreply.github.com>
2020-07-05 13:39:54 +0530

    General-Web UI-Enhancement-TypeAhead applied in rental page

---
+++ Vidly/Controllers/RentalsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class RentalsController : Controller
+    {
+        [Authorize(Roles = RoleName.MovieManager)]
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
